e9a8c40f5513b0a4cf9e28030e598128da5a4e31
Tim Aivazov <37597659+Aivazov@users.noreply.github.com>
2023-01-05 10:36:54 +0200

    learning example with timer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import HooksFirstMain from './Hooks-1/Main';
 import HooksFirstAlert from './Hooks-1/Alert';
+import Hook_2 from './Hooks-2/Hook_2';
 
 import './App.css';
 
@@ -10,12 +11,13 @@ function App() {
   const [alert, setAlert] = useState(false);
   const toggleAlert = () => setAlert((prev) => !prev);
   return (
-    <AlertContext.Provider value={alert}>
-      <div className="App">
-        <HooksFirstAlert />
-        <HooksFirstMain toggle={toggleAlert} />
-      </div>
-    </AlertContext.Provider>
+    // <AlertContext.Provider value={alert}>
+    //   <div className="App">
+    //     <HooksFirstAlert />
+    //     <HooksFirstMain toggle={toggleAlert} />
+    //   </div>
+    // </AlertContext.Provider>
+    <Hook_2 />
   );
 }
 
---
+++ src/Hooks-2/Hook_2.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+
+const styles = {
+  clockface: {
+    fontSize: 64,
+    fontWeight: 500,
+    textAlign: 'center',
+  },
+};
+
+export default class Hook_2 extends Component {
+  state = {
+    time: new Date(),
+    toggleBtn: true,
+  };
+
+  intervalId = null;
+
+  componentDidMount() {
+    // this.intervalId = setInterval(() => {
+    //   this.setState({ time: new Date() });
+    // }, 1000);
+    this.start();
+  }
+
+  componentWillUnmount() {
+    this.stop();
+  }
+
+  stop = () => {
+    clearInterval(this.intervalId);
+    this.setState({ toggleBtn: true });
+  };
+
+  start = () => {
+    this.intervalId = setInterval(() => {
+      this.setState({ time: new Date() });
+    }, 1000);
+    this.setState({ toggleBtn: false });
+  };
+
+  render() {
+    return (
+      <>
+        <p style={styles.clockface}>
+          Current time: {this.state.time.toLocaleTimeString()}
+        </p>
+        {!this.state.toggleBtn ? (
+          <button
+            type="button"
+            onClick={this.stop}
+            style={{
+              display: 'block',
+              marginLeft: 'auto',
+              marginRight: 'auto',
+            }}
+          >
+            Stop
+          </button>
+        ) : (
+          <button
+            type="button"
+            onClick={this.start}
+            style={{
+              display: 'block',
+              marginLeft: 'auto',
+              marginRight: 'auto',
+            }}
+          >
+            Start
+          </button>
+        )}
+      </>
+    );
+  }
+}
